dc5672ec2c5f66bbf7a98a7d542abb7d45666450
Tom Lackemann <tommylackemann@gmail.com>
2014-06-11 19:28:58 -0400

    complete refactoring of codebase - experimental right now but a lot better than what init.js was offering

---
+++ app/core/bootstrap.js
@@ -0,0 +1,43 @@
+/**
+ * Hype Commerce
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Hype Commerce Creative Commons License that is bundled with
+ * this package in the file LICENSE.txt. It is also available through the world-wide-web at this
+ * URL {@link http://www.hypecommerce.com/license}. If you did not receive a copy of the license
+ * and are unable to obtain it through the world-wide-web, please send an email to
+ * {@link mailto:license@hypecommerce.com} so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Hype Commerce to newer versions in the
+ * future. If you wish to customize Hype Commerce for your needs please refer to
+ * {@link http://www.hypecommerce.com/} for more information.
+ *
+ * @package		Hype
+ * @version		0.0.1.0
+ * @author		Hype Commerce Team <team@hypecommerce.com>
+ * @copyright	Copyright (c) 2014, Hype Commerce, Inc. (http://www.hypecommerce.com/)
+ * @license		http://www.hypecommerce.com/license
+ */
+
+var	fs      = require('fs'),
+    url     = require('url'),
+    when    = require('when');
+
+exports.loadConfiguration = function() {
+	// Promise we'll finish
+	var loaded = when.defer();
+
+	fs.exists("app/core/config.js", function checkConfiguration(fileExists) {
+		if (fileExists) {
+			loaded.resolve();
+		} else {
+			loaded.reject();
+		}
+	});
+
+    // Fufill the promise
+    return loaded.promise;
+};
