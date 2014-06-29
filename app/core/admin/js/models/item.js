b7641f27e16ad389e7094aa3e28914b282243fc3
Kurtis Kemple <kkemple@kurtiss-air.home>
2014-06-29 17:41:33 -0400

    cleaned up admin and set up for development

--- app/core/admin/js/models/item.js
+++
@@ -1,9 +0,0 @@
-define(["lib/backbone"], function(Backbone) {
-  var Item = Backbone.Model.extend({
-    defaults: {
-      price: 35,
-      photo: "http://www.placedog.com/100/100"
-    }
-  });
-  return Item;
-});
