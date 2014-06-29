3940d6b8e103c58c42a801baceebb6af8e99f50d
Tom Lackemann <tommylackemann@gmail.com>
2014-06-29 13:20:44 -0400

    core install script kind of working

--- app/plugins/hype/core/index.js
+++ app/plugins/hype/core/index.js
@@ -3,7 +3,7 @@ var Core;
 
 Core = {
 	name: 'Core',
-	version: '1.0.0.5',
+	version: '1.0.0.0',
 	enabled: true,
 	depends: {
 	},
--- app/plugins/hype/core/install/1.0.0.0.js
+++ app/plugins/hype/core/install/1.0.0.0.js
@@ -1,80 +1,43 @@
-var when = require('when');
-// Should seed necessary data once
 var Install = function(Hype) {
 
 	this.up = function() {
-		var loaded = when.defer();
 		Hype.Log.log('Installing Core-1.0.0.0');
 
-		// var Currency = Hype.Model.currency,
-		// 	Language = Hype.Model.language,
-		// 	View = Hype.Model.view;
+		var Currency = Hype.Model.currency,
+			Language = Hype.Model.language,
+			View = Hype.Model.view
+			Store = Hype.Model.store;
 
-		// // Adds currencies
-		// var currency = new Currency({
-		// 	name: 'US Dollar',
-		// 	character: '$',
-		// 	decimals: 2
-		// });
-		// currency.save();
+		// Adds currency
+		var currency = Currency.Db.create({
+			name: 'US Dollar',
+			character: '$',
+			decimals: 2
+		});
 
-		// console.log(currency);
+		// Add language
+		var languge = Language.Db.create({
+			name: 'English (US)',
+			code: 'en_US'
+		});
 
-		// // new Currency({
-		// // 	name: 'Euro',
-		// // 	character: '€',
-		// // 	decimals: 2
-		// // }).save();
+		// Add View
+		var view = View.Db.create({
+			name: 'My Store English', // English
+			code: 'store-en_US', // en_US | de_DE
+			url: 'http://localhost:5000/', // http://www.hypecommerce.com/ | http://www.hypecommerce.de/
+			language: 'en_US', // en_US | de_DE
+			primary: true, // true
+			currency: [currency]
+		});
 
-		// // new Currency({
-		// // 	name: 'Bitcoin',
-		// // 	character: 'B',
-		// // 	decimals: 8
-		// // }).save();
+		var store = Store.Db.create({
+			name: 'My Store', // My Store
+			code: 'store', // default
+			views: [view], // [en_US, en_UK, de_DE, fr_FR, es_ES]
+		});
 
-		// // // Add languages
-		// // new Language({
-		// // 	name: 'English (US)',
-		// // 	code: 'en_US'
-		// // }).save();
-
-		// // new Language({
-		// // 	name: 'English (UK)',
-		// // 	code: 'en_UK'
-		// // }).save();
-
-		// // new Language({
-		// // 	name: 'German',
-		// // 	code: 'de_DE'
-		// // }).save();
-
-		// // new Language({
-		// // 	name: 'Pirate',
-		// // 	code: 'ar_RG'
-		// // }).save();
-
-		// //Add View
-		// var view = new View({
-		// 	name: 'My Store English', // English
-		// 	code: 'store-en_US', // en_US | de_DE
-		// 	url: 'http://localhost:5000/', // http://www.hypecommerce.com/ | http://www.hypecommerce.de/
-		// 	language: 'en_US', // en_US | de_DE
-		// 	primary: true, // true
-		// 	currency: currency
-		// });
-		// view.save();
-
-		// console.log(view);
-
-		// // var store = new Store({
-		// // 	name: 'My Store', // My Store
-		// // 	code: 'store', // default
-		// // 	views: [view], // [en_US, en_UK, de_DE, fr_FR, es_ES]
-		// // });
-		loaded.resolve();
 		Hype.Log.log("Done install Core-1.0.0.0");
-
-		return loaded.promise;
 	},
 
 	this.down = function() {
