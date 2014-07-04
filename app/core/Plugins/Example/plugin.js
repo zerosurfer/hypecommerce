cb4cc22a5bed44ba2686219dd4ff50216a1cf339
Kurtis Kemple <kurtiskemple@gmail.com>
2014-07-03 22:09:36 -0400

    fixed messy code in plugin.js in Example plugin

--- app/core/Plugins/Example/plugin.js
+++ app/core/Plugins/Example/plugin.js
@@ -1,14 +1,12 @@
 module.exports = {
-    name = 'Category',
-    enabled = false,
-    depends = {
+    name: 'Category',
+    enabled: false,
+    depends: {
         'core': '>=1.0.0.0'
     },
-    creator = require('./lib/example'),
-    models = require('./lib/models'),
-    routes = require('./lib/routes'),
-    //scripts = require('./lib/scripts'),
-    version = '0.0.0.1';
-
-    return this;
+    creator: require('./lib/example'),
+    models: require('./lib/models'),
+    routes: require('./lib/routes'),
+    //scripts: require('./lib/scripts'),
+    version: '0.0.0.1'
 };
