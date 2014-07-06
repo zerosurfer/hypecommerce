7706bd609898b37462a801d692597e34f46af51c
Tom Lackemann <tommylackemann@gmail.com>
2014-07-06 17:06:49 -0400

    experimenting with some admin layout work

---
+++ app/core/Hype/Admin.js
@@ -0,0 +1,19 @@
+/**
+ * Hype Commerce
+ *
+ * @package		Hype
+ * @version		0.0.1.0
+ * @author		Hype Commerce Team <team@hypejs.com>
+ * @copyright	Copyright (c) 2014, Hype Commerce, Inc. (http://www.hypejs.com/)
+ * @license		http://www.hypejs.com/license
+ */
+
+ var Admin;
+
+ module.exports = function(Hype) {
+ 	Admin = function() {
+
+ 	}
+
+ 	return Admin;
+ }
