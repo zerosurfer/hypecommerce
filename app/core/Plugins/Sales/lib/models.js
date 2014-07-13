6b4cd0a2135cbb97c9dddda83c9fab6053a90a36
Tom Lackemann <tommylackemann@gmail.com>
2014-07-13 13:07:15 -0400

    added sales module for orders/invoices

---
+++ app/core/Plugins/Sales/lib/models.js
@@ -0,0 +1,71 @@
+module.exports = {
+    /**
+     * Order
+     *
+     * @var Number baseGrandTotal
+     * @var Number baseSubTotal
+     * @var Number baseItemTax
+     * @var Number baseShippingTax
+     * @var Number baseTaxTotal
+     * @var Number grandTotal
+     * @var Number subTotal
+     * @var Number itemTax
+     * @var Number shippingTax
+     * @var Number taxTotal
+     * @var String status
+     * @var Store store
+     * @var Customer customer
+     * @var Address shippingAddress
+     * @var Address billingAddress
+     * @var Date createdAt
+     * @var Date updatedAt
+     */
+    Order: {
+        schema: {
+            baseGrandTotal: Number,
+            baseSubTotal: Number,
+            baseItemTax: Number,
+            baseShippingTax: Number,
+            baseTaxTotal: Number,
+            grandTotal: Number,
+            subTotal: Number,
+            itemTax: Number,
+            shippingTax: Number,
+            taxTotal: Number,
+            status: String,
+            createdAt: Date,
+            updatedAt: Date
+        },
+        deps: {
+            hasOne: {
+                store: 'Store',
+                customer: 'Customer',
+                shippingAddress: 'Address',
+                billingAddress: 'Address'
+            }
+        }
+    },
+
+    /**
+     * Invoice
+     *
+     * @var Number baseTotalPiad
+     * @var Number totalPaid
+     * @var Date createdAt
+     * @var Date updatedAt
+     *
+     */
+    Invoice: {
+        schema: {
+            baseTotalPaid: Number,
+            totalPaid: Number,
+            createdAt: Date,
+            updatedAt: Date
+        },
+        deps: {
+            hasOne: {
+                order: 'Order'
+            }
+        }
+    }
+};
