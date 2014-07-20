982a5c5e286c1625cc12decbc747b87d68a6e28c
Tom Lackemann <tommylackemann@gmail.com>
2014-07-20 15:15:44 -0400

    backboning - cleaner than it sounds, adding a few more links to the sidebar (Store Rewards)

--- app/admin/static/main.js
+++ app/admin/static/main.js
@@ -1,12 +1,44 @@
 require.config({
-  paths: {
-    'jquery': 'components/jquery/dist/jquery',
-    'underscore': 'components/underscore/underscore',
-    'backbone': 'components/backbone/backbone',
-    'marionette': 'components/marionette/lib/backbone.marionette'
-  }
+
+	paths: {
+		'jquery': 'components/jquery/dist/jquery',
+		'underscore': 'components/underscore/underscore',
+		'backbone': 'components/backbone/backbone',
+		'marionette': 'components/marionette/lib/backbone.marionette'
+	},
+
+	shim: {
+		underscore: {
+			exports: '_'
+		},
+		backbone: {
+			deps: ['underscore', 'jquery'],
+			exports: 'Backbone',
+
+		},
+		marionette: {
+			exports: 'Backbone.Marionette',
+			deps: ['backbone']
+		}
+	},
+
+	deps: ['jquery', 'underscore']
 });
 
-require(['views/app'], function(AppView) {
+require([
+	'app',
+	'backbone',
+	'routers/index',
+	'controllers/index',
+	'views/app'
+], function (app, Backbone, Router, Controller) {
+	'use strict';
+
+	app.start();
+
+	new Router({ controller: Controller });
+
+	Backbone.history.start();
+
 	new AppView;
 });
