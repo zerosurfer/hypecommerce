2b14e8f96503adefae01532c1e83f82f95f99f70
Tom Lackemann <tommylackemann@gmail.com>
2014-07-20 20:51:45 -0400

    when we get to search, the search should find all settings pages, customers, or orders, it's already extendable so we're going to use it for third-party modules to extend as well

--- app/core/Plugins/System/admin.js
+++ app/core/Plugins/System/admin.js
@@ -13,6 +13,10 @@ module.exports = {
     menu: {
     	dashboard: {
     		label: "Dashboard",
+            // tags used for searching
+            tags: [
+                "dashboard"
+            ],
             sort: 10,
     		icon: "\f0e4",
     		url: "/dashboard"
@@ -21,6 +25,9 @@ module.exports = {
         help: {
             label: "Help",
             sort: 900,
+            tags: [
+                "faq", "help", "information", "info"
+            ],
             icon: "\f0e4",
             url: "/help"
         },
@@ -29,6 +36,9 @@ module.exports = {
             label: "Reports",
             sort: 120,
             icon: "\f080",
+            tags: [
+                "orders", "reports", "report", "customers"
+            ],
             url: "/reports"
         },
 
@@ -37,6 +47,9 @@ module.exports = {
             sort: 0,
             icon: "\f188",
             url: "/developer",
+            tags: [
+                "developer", "developer mode"
+            ],
             class: "list-group-item-danger",
             visible: true, // should load a model to determine if we're in "Developer Mode" or not
         },
@@ -51,12 +64,18 @@ module.exports = {
                     label: "Manage Plugins",
                     sort: 100,
                     icon: "\f141",
+                    tags: [
+                        "plugins", "manage", "enable", "disable"
+                    ],
                     url: "/plugins/manage"
                 },
                 find: {
                     label: "Find New Plugins",
                     sort: 100,
                     icon: "\f0ed",
+                    tags: [
+                        "new", "plugins", "install", "find"
+                    ],
                     url: "/plugins/find"
                 }
             }
@@ -70,12 +89,18 @@ module.exports = {
             children: {
                 admin: {
                     label: "Admin",
+                    tags: [
+                        "admin", "users", "groups", "permissions", "access", "control"
+                    ],
                     sort: 100,
                     icon: "\f023",
                     url: "/configuration/admin"
                 },
                 security: {
                     label: "Security",
+                    tags: [
+                        "captcha", "security", "keys", "oauth", "permissions", "groups"
+                    ],
                     sort: 200,
                     icon: "\f0e3",
                     url: "/configuration/security"
