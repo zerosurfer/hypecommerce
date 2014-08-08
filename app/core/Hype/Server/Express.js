bc8981b05fc2ad54ca7c339b679cacec033d978a
Tom Lackemann <tommylackemann@gmail.com>
2014-08-07 23:08:26 -0400

    when posting to /setup/install it generates a config file - should be a minified name file (?) - should Hype initializer check for a min file?

--- app/core/Hype/Server/Express.js
+++ app/core/Hype/Server/Express.js
@@ -1,4 +1,6 @@
 var path = require('path'),
+	fs = require('fs'),
+    crypto = require('crypto'),
 	express = require('express'),
 	app = express();
 
@@ -62,6 +64,64 @@ module.exports = function(Hype) {
 				app.use('/setup/', express.static(path.resolve(__dirname + '/../../../setup')));
 				app.get('/setup', function (req, res) {
 					res.render('index.html');
+				});
+
+				// Add the server api call, we're going to make a POST request with the data that will generate the config.js file
+				app.post('/setup/install', function (req, res) {
+					// Create the secret key
+					var md5Hash = crypto.createHash('md5');
+
+					// Create the JSON config file
+					var config = {
+						version: Config.version,
+						install: new Date(),
+						secret: md5Hash.update(Math.random() + ":" + install).digest('hex'),
+						environment: "hype",
+						debug: true,
+						log: true,
+						hype: {
+							db: {
+								type: "mongo",
+								mongo: {
+									host: "127.0.0.1",
+									port: 27017,
+									username: "",
+									password: "",
+									dbname: "hype_development"
+								}
+							},
+							server: {
+								type: 'express',
+								admin: "/admin", // url for the admin
+								express: {
+									url: "http://localhost",
+									port: 4973, // "HYPE" on a phone keypad =)
+									nodes: 2, // for clustering
+									theme: "ractive" // theme is specific, since we're frontend agnostic, who says we even need one
+									// express options should go here
+								},
+								https: {
+									admin: 'admin.{url}',
+									port: 4443
+								},
+								session: {
+									storage: "redis",
+									redis: {
+										host: "localhost",
+										port: 6379,
+										db: "hype_development",
+										pass: "u2T2BvKcXXGk9pKXjfXw",
+										secret: "XMGnKPtQEuZbWYWLp5CZ",
+										cookie: { path: "/", maxAge: 3600000 }
+									}
+								}
+							}
+						}
+					}
+
+					fs.writeFileSync('app/config.js', "module.exports=" + JSON.stringify(config));
+					res.status(200);
+					res.send({ success: 'Config file created, redirecting you to your store' });
 				});
 			}
 
