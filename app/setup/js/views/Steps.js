b32f16a11cacde05dac379656cdc67e44a775cdf
Tom Lackemann <tommylackemann@gmail.com>
2014-08-07 22:23:02 -0400

    backbone is actually switching views! who the hell knows if the form works but it's switching views!!

--- app/setup/js/views/Steps.js
+++ app/setup/js/views/Steps.js
@@ -17,6 +17,75 @@ define([
 			step: '#current-step'
 		},
 
+		currentStep: 0,
+
+		steps: [
+			'store',
+			'database',
+			'server',
+			'plugins',
+			'review'
+		],
+
+		events: {
+		    'click .next-step': 'clickNextStep',
+		    'click .prev-step': 'clickPrevStep',
+		},
+
+		clickNextStep: function(e) {
+			e.preventDefault();
+			var Step = function() {};
+
+			this.currentStep++;
+
+			switch(this.steps[this.currentStep]) {
+				case 'store' :
+					Step = new StoreStep();
+					break;
+				case 'database' :
+					Step = new DatabaseStep();
+					break;
+				case 'server' :
+					Step = new ServerStep();
+					break;
+				case 'plugins' :
+					Step = new PluginStep();
+					break;
+				case 'review' :
+					Step = new ReviewStep();
+					break;
+			}
+
+			this.step.show(Step);
+		},
+
+		clickPrevStep: function(e) {
+			e.preventDefault();
+			var Step = function() {};
+
+			this.currentStep--;
+
+			switch(this.steps[this.currentStep]) {
+				case 'store' :
+					Step = new StoreStep();
+					break;
+				case 'database' :
+					Step = new DatabaseStep();
+					break;
+				case 'server' :
+					Step = new ServerStep();
+					break;
+				case 'plugins' :
+					Step = new PluginStep();
+					break;
+				case 'review' :
+					Step = new ReviewStep();
+					break;
+			}
+
+			this.step.show(Step);
+		},
+
 		onRender: function () {
 			console.log('updating the current step');
 			this.step.show(new StoreStep());
