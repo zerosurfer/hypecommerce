4aed26e9aba294716e3b0e37ad5cd88503383408
Tom Lackemann <tommylackemann@gmail.com>
2014-08-22 17:05:33 -0400

    added some documentation for Cms models

--- app/core/Plugins/Cms/lib/models.js
+++ app/core/Plugins/Cms/lib/models.js
@@ -11,11 +11,24 @@
 
 module.exports = {
 
+    /**
+     * CMS Block
+     *
+     * @var String name; The name of the static block
+     * @var String code; The unique identifing code
+     * @var String description; A short and sweet description for your admin users
+     * @var String content; A combination of text and HTML content
+     * @var Boolean enabled; A disabled block will not render
+     * @var Store store; The store association (if any, can cross all stores)
+     * @var Date createdAt
+     * @var Date updatedAt
+     */
     CmsBlock: {
         schema: {
             name: String,
             description: String,
             content: String,
+            enabled: Boolean,
             createdAt: Date,
             updatedAt: Date
         },
@@ -26,18 +39,35 @@ module.exports = {
         }
     },
 
+    /**
+     * Page
+     *
+     * @var String title; The <head> title of the page 
+     * @var String code; The unique identifing code
+     * @var String content; A combination of text and HTML
+     * @var String metaDescription: The meta description of the page
+     * @var Array metaKeywords: The meta tags of the page
+     * @var Boolean published; Unpublished pages will not render or return 404 when called
+     * @var Store store; The store association (if any, can cross all stores)
+     * @var Media ogImage; The image to load when sharing with Facebook (optional)
+     * @var Date createdAt
+     * @var Date updatedAt
+     */
     Page: {
         schema: {
-            name: String,
-            description: String,
+            title: String,
+            code: String,
             content: String,
             metaDescription: String,
-            metaTags: String,
+            metaKeywords: [String],
+            published: Boolean,
+            publishedAt: Date,
             createdAt: Date,
             updatedAt: Date
         },
         deps: {
             hasOne: {
+                ogImage: 'Media', // for facebook og:image tag
                 store: 'Store'
             }
         }
